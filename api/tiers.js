42f8093fdd944dbd19b81bda825903e684cad53d
Caltex254 <caltex423@gmail.com>
2026-06-27 22:56:38 +0300

    fix: remove api/tiers.js to fit Vercel Hobby 12-function limit (admin dashboard needs the slot)

--- api/tiers.js
+++
@@ -1,9 +0,0 @@
-// GET /api/tiers
-const { TIERS, MIN_WITHDRAWAL } = require('./_lib/config');
-const { setCORS, sendJson } = require('./_lib/utils');
-
-module.exports = (req, res) => {
-  setCORS(res);
-  if (req.method === 'OPTIONS') return res.end();
-  return sendJson(res, 200, { tiers: TIERS, minWithdrawal: MIN_WITHDRAWAL });
-};
